9dff24990ec020c3cbeb2c083def48bf98176fad
James Rogers <jarogers095@gmail.com>
2020-01-24 04:39:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,10 +12,10 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(words)
-  if(words == words.upcase)
-    return "NO, NOT SINCE 1938!"
-  elsif(words == "I LOVE YOU GRANDMA!")
+  if(words == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif(words == words.upcase)
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH? SPEAK UP, SONNY!"
   end
