1a18ffda304ec82b65b5dd423ba51ea8208a7f90
James Rogers <jarogers095@gmail.com>
2020-01-24 04:39:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,6 @@ def speak_to_grandma(words)
   elsif(words == words.upcase)
     return "NO, NOT SINCE 1938!"
   else
-    return "HUH? SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
